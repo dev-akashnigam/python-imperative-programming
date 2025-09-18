22ddb10606ee158180a52dde82f82d36e9b55c2f
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-18 16:39:18 +0530

    Imperative Python solution for printing the sum, difference, product and quotient of two numbers entered by the user

---
+++ 001_sum_difference_product_quotient_of_2_nos.py
@@ -0,0 +1,17 @@
+# Read two numbers and print their sum, difference, product, and quotient.
+
+print("Please enter first number: ")
+first_num = int(input())
+
+print("Please enter second number: ")
+second_num = int(input())
+
+sum = first_num + second_num
+diff = first_num - second_num
+product = first_num * second_num
+quotient = first_num / second_num
+
+print(f"Sum = {sum}")
+print(f"Difference = {diff}")
+print(f"Product = {product}")
+print(f"Quotient = {quotient}")
